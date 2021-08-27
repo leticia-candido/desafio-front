59547c5cc8368c0ea16d906cfacb0e72f85cbb66
Leticia-Candido <leticiacandidoingles@gmail.com>
2021-08-26 23:35:16 -0300

    index mobile and email

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ function productCard(product){
                 <div class="product-image">
                     <img class="image" src="http:${product.image}" alt="product">
                 </div>
+                <div class="product-content">
                 <h1>${product.name}</h1>
-                <p>${product.description}</p>
-                <p>De: ${currencyBrOldPrice}</p>
-                <span>Por: ${currencyBrPrice}</span>
-                <p>ou ${product.installments.count}x de ${currencyBrPInstallments}</p>
+                <p class="description">${product.description}</p>
+                <p class="old-price">De: ${currencyBrOldPrice}</p>
+                <span class="price">Por: ${currencyBrPrice}</span>
+                <p class="installments">ou ${product.installments.count}x de ${currencyBrPInstallments}</p>
                 <button class="btn-buy" type="button">Comprar</button>
+                </div>
     `
 }
 
@@ -61,12 +63,66 @@ let userFriendForm = {
 
 function submitUserMainForm(event){
     event.preventDefault();
+
     userMainForm.name = document.querySelector("#register-first-name").value;
     userMainForm.email = document.querySelector("#register-email").value;
     userMainForm.cpf = document.querySelector("#register-cpf").value;
+    
     if(!validateCpf(userMainForm.cpf)){
-        return document.querySelector("#validate-cpf").textContent = "CPF inválido";
-        //add code to predict delete invalid text
-    };
+        document.querySelector("#validate-cpf").textContent = "CPF inválido";
+        return;
+    }
+
+    document.querySelector("#validate-cpf").textContent = "";
+
     userMainForm.gender = document.querySelector('input[name="gender"]:checked').value;    
+
+    const submitUserButton = document.querySelector("#user-form-submit");
+    submitUserButton.textContent = "Enviado!";
+    submitUserButton.disabled = true;
+
+    const userForm = document.querySelector("#user-form");
+
+    const inputs = userForm.querySelectorAll('input');
+    
+    inputs.forEach(input => {
+        input.disabled = true;
+    });
 }
+
+function submitFriendForm(event){
+    event.preventDefault();
+
+    if (!userMainForm.name || !userMainForm.gender) {
+        document.querySelector('#friend-form-msg').textContent = "Preencha o formulário principal";
+        return
+    }
+
+    document.querySelector('#friend-form-msg').textContent = "";
+
+    userFriendForm.name = document.querySelector("#register-friend-name").value;
+    userFriendForm.email = document.querySelector("#register-friend-email").value;
+
+    const friendSubmitButton = document.querySelector('#friend-form-submit');
+    friendSubmitButton.textContent = 'Enviado!'
+    friendSubmitButton.disabled = true;
+
+    setTimeout(() => {
+        friendSubmitButton.textContent = 'Enviar agora'
+        friendSubmitButton.disabled = false;
+        
+        const friendForm = document.querySelector("#friend-form");
+        const inputs = friendForm.querySelectorAll('input');
+
+        inputs[0].focus();
+        
+        inputs.forEach(input => {
+            input.value = "";
+        });
+
+    }, 3000);
+
+}
+
+
+
